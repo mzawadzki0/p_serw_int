375cad4c1c3d02347ce2ba0da64664e1388459a3
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-11-05 23:49:19 +0100

    naprawione models.py

--- project/project/twitter2/models.py
+++ project/project/twitter2/models.py
@@ -8,12 +8,12 @@ class User(models.Model):
     created_datetime = models.DateTimeField()
 
 class Following(models.Model):
-    user_id_follower = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_id_followed = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id_follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    user_id_followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
     datetime = models.DateTimeField()
 
 class Post(models.Model):
-    class PostVilibility(models.TextChoices):
+    class PostVisibility(models.TextChoices):
         PUBLIC = 'P'
         FOLLOWERS = 'F'
 
@@ -21,7 +21,7 @@ class Post(models.Model):
     datetime = models.DateTimeField()
     title = models.CharField(max_length=80)
     content = models.CharField(max_length=500)
-    is_reply_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_reply_to = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True)
     visibility = models.CharField(max_length=1, choices=PostVisibility.choices, default=PostVisibility.PUBLIC)
 
 class LikeDislike(models.Model):
@@ -29,24 +29,24 @@ class LikeDislike(models.Model):
         LIKE = 'L'
         DISLIKE = 'D'
 
+    post_id = models.OneToOneField(Post, on_delete=models.CASCADE, primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post, unique=True, on_delete=models.CASCADE)
     datetime = models.DateTimeField()
-    like_dislike = models.TextChoices(max_length=1, choices=LikeDisl.choices, default=LikeDislike.LIKE)
+    like_dislike = models.CharField(max_length=1, choices=LikeDisl.choices, default=LikeDisl.LIKE)
 
 class Comment(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     datetime = models.DateTimeField()
     content = models.CharField(max_length=300)
-    is_reply_to = models.ForeignKey(Comment, on_delete=models.SET_NULL, null=True)
+    is_reply_to = models.ForeignKey('Comment', on_delete=models.SET_NULL, null=True)
 
 class LikeDislikeComment(models.Model):
     class LikeDisl(models.TextChoices):
         LIKE = 'L'
         DISLIKE = 'D'
 
+    post_id = models.OneToOneField(Comment, on_delete=models.CASCADE, primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Comment, unique=True, on_delete=models.CASCADE)
     datetime = models.DateTimeField()
-    like_dislike = models.TextChoices(max_length=1, choices=LikeDisl.choices, default=LikeDislike.LIKE)
+    like_dislike = models.CharField(max_length=1, choices=LikeDisl.choices, default=LikeDisl.LIKE)
