e3ed0aacc25a7eed2fddde890df7ff623b64207e
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-11-07 10:41:37 +0100

    admin py

--- project/project/twitter2/admin.py
+++ project/project/twitter2/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from twitter2.models import User, Post, Comment
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
