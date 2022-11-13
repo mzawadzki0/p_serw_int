6ae237030487d59bbf11a0c9d5b1430f28483e5f
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-11-13 20:32:13 +0100

    User.email z charField na emailField

--- project/project/twitter2/models.py
+++ project/project/twitter2/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
     username = models.CharField(max_length=40)
     password = models.CharField(max_length=60)
     created_time = models.DateTimeField(auto_now_add=True)
