7d144763e6027a4547669817421ff21b75b10c6d
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-11-13 20:32:50 +0100

    widok PostList i PostCreate

--- project/project/twitter2/urls.py
+++ project/project/twitter2/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('posts', views.PostList.as_view()),
+    path('posts/add', views.PostCreate.as_view()),
+    # path('', views.index, name='index'),
 ]
--- project/project/twitter2/views.py
+++ project/project/twitter2/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
 from django.http import HttpResponse
+from .models import User, Following, Post, LikeDislike, Comment, LikeDislikeComment
+from .serializers import UserSerializer, FollowingSerializer, PostSerializer, LikeDislikeSerializer, CommentSerializer, LikeDislikeCommentSerializer
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse('<h1>test<h1>')
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    name = 'post-list'
+    ordering_fields = ['created_time']
+
+class PostCreate(generics.CreateAPIView):
+    serializer_class = PostSerializer
+    name = 'post-create'
