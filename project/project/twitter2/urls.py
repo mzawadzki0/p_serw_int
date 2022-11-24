f2edb5550702b90c0c6713e3472398eb696ef52a
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2022-11-24 09:09:08 +0100

    dodano widoki

--- project/project/twitter2/urls.py
+++ project/project/twitter2/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('posts', views.PostList.as_view()),
     path('posts/add', views.PostCreate.as_view()),
+    path('comments', views.CommentList.as_view()),
+    path('comments/add', views.CommentCreate.as_view()),
     # path('', views.index, name='index'),
 ]
