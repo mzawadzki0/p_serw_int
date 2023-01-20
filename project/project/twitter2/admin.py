1642f32afd7d37111f7df0f6ebcc8f7e8f3adfca
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2023-01-20 04:27:21 +0100

    imported all models

--- project/project/twitter2/admin.py
+++ project/project/twitter2/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from twitter2.models import Post, Comment
+from .models import Post, Comment, Following, LikeDislike, LikeDislikeComment
 
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Following)
+admin.site.register(LikeDislike)
+admin.site.register(LikeDislikeComment)
