ecc61eead77c088b08c8441f82a3afa48ee20eda
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2023-01-22 17:23:02 +0100

    literówka chyba

--- project/project/project/urls.py
+++ project/project/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('twitter2/', include('twitter2.urls')),
     path('admin/', admin.site.urls),
-    path(r'^api-auth/', include('rest_framework.urls')),
+    path(r'api-auth/', include('rest_framework.urls'))
 ]
